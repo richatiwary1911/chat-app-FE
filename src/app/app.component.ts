2c7fe1077700fd06615133daa2f95f091e1abb7d
richatiwary1911 <richatiwary1911@gmail.com>
2020-10-30 18:37:19 +0530

    chat-app skeleton DONE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'chat-app';
+  title = 'Chit Chat';
+  message = '';
+  chats = ['Hi', 'Hello', 'How are you?', 'I am fine','What about you?', 'I am good'];
+
+  addChat() {
+    if(this.message.length == 0)
+    {
+      return;
+    }
+    this.chats.push(this.message);
+    this.message = '';
+
+    window.setInterval(() => {
+      const elem = document.getElementById('scrolldiv');
+      elem.scrollTop = elem.scrollHeight;
+    }, 500)
+  }
 }
